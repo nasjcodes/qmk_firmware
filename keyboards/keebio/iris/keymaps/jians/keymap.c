058d834e6ac2c72d43154faec9579599b4298ba1
Jian Sheng <nasjcodes@users.noreply.github.com>
2022-02-09 22:09:28 +0800

    keymap updates
    - set rgb for oneshot mods
    - shift to secondary thumb

--- keyboards/keebio/iris/keymaps/jians/keymap.c
+++ keyboards/keebio/iris/keymaps/jians/keymap.c
@@ -19,8 +19,8 @@ extern keymap_config_t keymap_config;
 /* Aliases */
 // Layers
 #define SPC_RSE LT(_RAISE, KC_SPC)
-#define TAB_LWR LT(_LOWER, KC_TAB)
-#define SPC_SFT MT(MOD_LSFT, KC_SPC)
+#define SPC_LWR LT(_LOWER, KC_SPC)
+#define TAB_SFT MT(MOD_LSFT, KC_TAB)
 #define BSP_NUM LT(_NUM, KC_BSPC)
 
 #define ESC_SFT MT(MOD_LSFT, KC_ESC)
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
         KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    DF(_GAME),        _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, OSL(_MISC),
     // └────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                       KC_LALT, TAB_LWR, SPC_SFT,                   SPC_RSE, BSP_NUM, KC_ENT
+                                       KC_LALT, TAB_SFT, SPC_LWR,                   SPC_RSE, BSP_NUM, KC_ENT
                                 //    └────────┴────────┴────────┘                 └────────┴────────┴────────┘
     ),
 
@@ -92,9 +92,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // ┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
         _______, _______, _______, _______, _______, A(KC_F4),                           _______, _______, _______, _______, _______, _______,
     // ├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-        _______, _______, SW_WIN,  _______, _______, _______,                            KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_INS,  _______,
+        _______, _______, SW_WIN,  _______, _______, _______,                            KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_INS,  KC_BSPC,
     // ├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-        _______, KC_LGUI, KC_LCTL, KC_LSFT, KC_LALT,  SAVE,                              KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT,KC_PSCR, _______,
+        _______, KC_LGUI, OS_CTL,  OS_SFT,  OS_ALT,  SAVE,                              KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT,KC_PSCR, KC_DEL,
     // ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
         _______, UNDO,    CUT,     COPY,    PASTE,   REDO,    _______,          _______, _______, _______, _______, _______, _______, _______,
     // └────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
@@ -108,9 +108,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // ├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
         _______, _______, KC_AMPR, KC_ASTR, KC_TILD, KC_UNDS,                            KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, _______, _______,
     // ├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-        _______, _______, KC_DLR,  KC_PERC, KC_CIRC, KC_MINS,                            KC_EQL,  KC_LPRN, KC_RPRN, KC_BSLS, _______, _______,
+        _______, _______, KC_DLR,  KC_PERC, KC_CIRC, KC_MINS,                            KC_EQL,  KC_LPRN, KC_RPRN, KC_BSLS, KC_COLN, _______,
     // ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-        _______, _______, KC_EXLM, KC_AT,   KC_HASH, KC_GRV,  _______,          _______, KC_LBRC, KC_RBRC, KC_LABK, KC_RABK, _______, _______,
+        _______, _______, KC_EXLM, KC_AT,   KC_HASH, KC_GRV,  _______,          _______, KC_LBRC, KC_RBRC, KC_LABK, KC_RABK, KC_QUES, _______,
     // └────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                        _______, _______, _______,                   _______, _______,  _______
                                 //    └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -122,7 +122,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // ├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
         _______, _______, _______, _______, _______, _______,                            _______, KC_7,    KC_8,    KC_9,    _______, _______,
     // ├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-        _______, KC_LGUI, KC_LCTL, KC_LSFT, KC_LALT, _______,                            _______, KC_4,    KC_5,    KC_6,    _______, _______,
+        _______, KC_LGUI, OS_CTL,  OS_SFT,  OS_ALT,  _______,                            _______, KC_4,    KC_5,    KC_6,    _______, _______,
     // ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
         _______, _______, _______, _______, _______, _______, _______,          _______, KC_0,    KC_1,    KC_2,    KC_3,    _______, _______,
     // └────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
@@ -159,12 +159,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
-// keep the thumb cluster "responsive" i.e cannot double tap to hold tap action
+// cannot double tap to hold tap action
+// For entire thumb cluster
 bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case SPC_RSE:
-        case TAB_LWR:
-        case SPC_SFT:
+        case SPC_LWR:
+        case TAB_SFT:
         case BSP_NUM:
             return true;
         default:
@@ -172,11 +173,14 @@ bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
     }
 }
 
-// keep the SECONDARY thumb cluster "responsive" i.e able to tap keys immediately without waiting for release
+// Able to tap keys immediately without waiting for release (permissive hold still requires key to be released when within tapping term)
+// For secondary thumb keys
+// include main left thumb since space is rarely used
 bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case TAB_LWR:
+        case TAB_SFT:
         case BSP_NUM:
+        case SPC_LWR:
             // Immediately select the hold action when another key is pressed.
             return true;
         default:
@@ -198,12 +202,15 @@ bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
     }
 }
 
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, _LOWER, _RAISE, _FUNC);
+}
 
 /* Oneshot config */
 bool is_oneshot_cancel_key(uint16_t keycode) {
     switch (keycode) {
     case SPC_RSE:
-    case SPC_SFT:
+    case SPC_LWR:
         return true;
     default:
         return false;
@@ -213,7 +220,7 @@ bool is_oneshot_cancel_key(uint16_t keycode) {
 bool is_oneshot_ignored_key(uint16_t keycode) {
     switch (keycode) {
         case SPC_RSE:
-        case SPC_SFT:
+        case SPC_LWR:
         case KC_LSFT:
         case OS_SFT:
         case OS_CTL:
@@ -225,10 +232,32 @@ bool is_oneshot_ignored_key(uint16_t keycode) {
     }
 }
 
-oneshot_state os_shft_state = os_up_unqueued;
-oneshot_state os_ctrl_state = os_up_unqueued;
-oneshot_state os_alt_state = os_up_unqueued;
-oneshot_state os_cmd_state = os_up_unqueued;
+const ONESHOT shft = {
+    state: os_up_unqueued,
+    KC_LSFT,
+    OS_SFT,
+    led_index: 15
+};
+const ONESHOT ctrl = {
+    state: os_up_unqueued,
+    KC_LCTL,
+    OS_CTL,
+    led_index: 14
+};
+const ONESHOT alt = {
+    state: os_up_unqueued,
+    KC_LALT,
+    OS_ALT,
+    led_index: 16
+};
+const ONESHOT cmd = {
+    state: os_up_unqueued,
+    KC_LGUI,
+    OS_GUI,
+    led_index: 13
+};
+
+ONESHOT osm_arr[] = { shft, ctrl, alt, cmd };
 
 /* Swapper config */
 bool sw_win_active = false;
@@ -297,27 +326,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return false;
     }
 
-    update_swapper(
-        &sw_win_active, KC_LALT, KC_TAB, SW_WIN,
-        keycode, record
-    );
-
-    update_oneshot(
-        &os_shft_state, KC_LSFT, OS_SFT,
-        keycode, record
-    );
-    update_oneshot(
-        &os_ctrl_state, KC_LCTL, OS_CTL,
-        keycode, record
-    );
-    update_oneshot(
-        &os_alt_state, KC_LALT, OS_ALT,
-        keycode, record
-    );
-    update_oneshot(
-        &os_cmd_state, KC_LCMD, OS_GUI,
-        keycode, record
-    );
+    for (int i = 0; i < sizeof(osm_arr) / sizeof(*osm_arr); i++) {
+        update_oneshot(
+            &osm_arr[i],
+            keycode, record
+        );
+    }
 
     return true;
 }
@@ -356,8 +370,14 @@ layer_state_t default_layer_state_set_user(layer_state_t state) {
 }
 
 void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-}
+    HSV hsv = { HSV_SECONDARY };
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _LOWER, _RAISE, _FUNC);
+    RGB rgb = hsv_to_rgb(hsv);
+
+    for (int i = 0; i < sizeof(osm_arr) / sizeof(*osm_arr); i++) {
+        if (osm_arr[i].state == os_up_queued) {
+            rgb_matrix_set_color(osm_arr[i].led_index, rgb.r, rgb.g, rgb.b);
+        }
+    }
+
 }
