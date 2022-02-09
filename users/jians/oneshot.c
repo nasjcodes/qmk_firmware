a5d7ab89c7d75c790e707d75643abab7ee2f2457
Jian Sheng <nasjcodes@users.noreply.github.com>
2022-02-09 22:08:42 +0800

    update oneshot to use singular ONESHOT struct

--- users/jians/oneshot.c
+++ users/jians/oneshot.c
@@ -1,30 +1,28 @@
 #include "oneshot.h"
 
 void update_oneshot(
-    oneshot_state *state,
-    uint16_t mod,
-    uint16_t trigger,
+    ONESHOT *oneshot,
     uint16_t keycode,
     keyrecord_t *record
 ) {
-    if (keycode == trigger) {
+    if (keycode == oneshot->trigger) {
         if (record->event.pressed) {
             // Trigger keydown
-            if (*state == os_up_unqueued) {
-                register_code(mod);
+            if (oneshot->state == os_up_unqueued) {
+                register_code(oneshot->mod);
             }
-            *state = os_down_unused;
+            oneshot->state = os_down_unused;
         } else {
             // Trigger keyup
-            switch (*state) {
+            switch (oneshot->state) {
             case os_down_unused:
                 // If we didn't use the mod while trigger was held, queue it.
-                *state = os_up_queued;
+                oneshot->state = os_up_queued;
                 break;
             case os_down_used:
                 // If we did use the mod while trigger was held, unregister it.
-                *state = os_up_unqueued;
-                unregister_code(mod);
+                oneshot->state = os_up_unqueued;
+                unregister_code(oneshot->mod);
                 break;
             default:
                 break;
@@ -32,21 +30,21 @@ void update_oneshot(
         }
     } else {
         if (record->event.pressed) {
-            if (is_oneshot_cancel_key(keycode) && *state != os_up_unqueued) {
+            if (is_oneshot_cancel_key(keycode) && oneshot->state != os_up_unqueued) {
                 // Cancel oneshot on designated cancel keydown.
-                *state = os_up_unqueued;
-                unregister_code(mod);
+                oneshot->state = os_up_unqueued;
+                unregister_code(oneshot->mod);
             }
         } else {
             if (!is_oneshot_ignored_key(keycode)) {
                 // On non-ignored keyup, consider the oneshot used.
-                switch (*state) {
+                switch (oneshot->state) {
                 case os_down_unused:
-                    *state = os_down_used;
+                    oneshot->state = os_down_used;
                     break;
                 case os_up_queued:
-                    *state = os_up_unqueued;
-                    unregister_code(mod);
+                    oneshot->state = os_up_unqueued;
+                    unregister_code(oneshot->mod);
                     break;
                 default:
                     break;
--- users/jians/oneshot.h
+++ users/jians/oneshot.h
@@ -10,13 +10,18 @@ typedef enum {
     os_down_used,
 } oneshot_state;
 
+typedef struct ONESHOT {
+    oneshot_state state;
+    uint16_t mod;
+    uint16_t trigger;
+    uint8_t led_index;
+} ONESHOT;
+
 // Custom oneshot mod implementation that doesn't rely on timers. If a mod is
 // used while it is held it will be unregistered on keyup as normal, otherwise
 // it will be queued and only released after the next non-mod keyup.
 void update_oneshot(
-    oneshot_state *state,
-    uint16_t mod,
-    uint16_t trigger,
+    ONESHOT *ONESHOT,
     uint16_t keycode,
     keyrecord_t *record
 );
