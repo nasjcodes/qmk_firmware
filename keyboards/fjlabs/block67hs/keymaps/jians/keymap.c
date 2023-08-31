bd38d5f15da22afabb5686734b62d9fd5672597c
Jian Sheng <nasjcodes@users.noreply.github.com>
2023-09-01 03:08:29 +0800

    update keymap for block67

--- keyboards/fjlabs/block67hs/keymaps/jians/keymap.c
+++ keyboards/fjlabs/block67hs/keymaps/jians/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LAYER0] = LAYOUT_wrapper(
         KC_GRV,  ________________NUMBER_LEFT________________, ________________NUMBER_RIGHT_______________, KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
         KC_TAB,  _________________QWERTY_L1_________________, _________________QWERTY_R1_________________, KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
-        ESC_1,  _________________QWERTY_L2_________________, _________________QWERTY_R2_________________,           KC_ENT,  KC_DEL,  KC_PGDN,
+        ESC_1,   _________________QWERTY_L2_________________, _________________QWERTY_R2_________________,          KC_ENT,  KC_DEL,  KC_PGDN,
         KC_LSFT, KC_LSFT, _________________QWERTY_L3_________________, _________________QWERTY_R3_________________, KC_RSFT, KC_UP,   KC_END,
         KC_LCTL, KC_LGUI, ALT_3,                         KC_SPC,                                 KC_RALT, MO(2),    KC_LEFT, KC_DOWN, KC_RGHT
     ),
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   KC_DEL,  KC_PSCR,
         KC_TRNS, ___________________BLANK___________________, KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_VOLU,
         KC_TRNS, ___________________BLANK___________________, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_VOLD,
-        KC_TRNS, KC_LSFT, ___________________BLANK___________________, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, ___________________BLANK___________________, ___________________BLANK___________________, KC_TRNS,  KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS,                       KC_TRNS,                                 KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS
     ),
 
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, ___________________BLANK___________________, ___________________BLANK___________________, KC_TRNS, KC_TRNS, KC_TRNS, QK_BOOT,
         KC_TRNS, ___________________BLANK___________________, ___________________BLANK___________________, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_CAPS, ___________________BLANK___________________, ___________________BLANK___________________, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_LSFT, ___________________BLANK___________________, ___________________BLANK___________________, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, ___________________BLANK___________________, ___________________BLANK___________________, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS,                       KC_TRNS,                                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, ___________________BLANK___________________, ___________________BLANK___________________, KC_TRNS, KC_TRNS, KC_TRNS, STR_TV,
         KC_TRNS, ___________________BLANK___________________, ___________________BLANK___________________, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_MPLY, ___________________BLANK___________________, ___________________BLANK___________________, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_LSFT, ___________________BLANK___________________, ___________________BLANK___________________, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, ___________________BLANK___________________, ___________________BLANK___________________, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS,                       KC_TRNS,                                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
