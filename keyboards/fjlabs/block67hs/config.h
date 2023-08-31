2eb197c679ff7fc926d92cd76ec5fd259f646711
Jian Sheng <nasjcodes@users.noreply.github.com>
2023-08-31 19:47:39 +0800

    fix block67 compilation

--- keyboards/fjlabs/block67hs/config.h
+++ keyboards/fjlabs/block67hs/config.h
@@ -17,15 +17,6 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "config_common.h"
-
-/* USB Device descriptor parameter */
-#define VENDOR_ID    0x7074 // FJLabs
-#define PRODUCT_ID   0x1021 // Block67 Hotswap
-#define DEVICE_VER   0x0001 // Version 1
-#define MANUFACTURER    FJLabs
-#define PRODUCT         Block67 Hotswap
-
 /* key matrix size */
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 15
@@ -43,7 +34,6 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #define MATRIX_ROW_PINS { F4, F1, F7, F6, F5 }
 #define MATRIX_COL_PINS { F0, B1, B2, B3, B7, D0, D1, D2, D3, D5, D7, B4, B5, B6, C6 }
-#define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
@@ -97,5 +87,5 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 //#define NO_ACTION_LAYER
 //#define NO_ACTION_TAPPING
 //#define NO_ACTION_ONESHOT
-#define NO_ACTION_MACRO
-#define NO_ACTION_FUNCTION
+// #define NO_ACTION_MACRO
+// #define NO_ACTION_FUNCTION
