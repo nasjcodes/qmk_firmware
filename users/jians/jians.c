11de34dbfab6b2bbc8c6e510829d3a62ebd1fd24
Jian Sheng <nasjcodes@users.noreply.github.com>
2023-08-31 18:44:34 +0800

    common config

--- users/jians/jians.c
+++ users/jians/jians.c
@@ -1,1 +1,20 @@
 #include "jians.h"
+// #include "print.h"
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    // uprintf("0x%04X keycode\n", keycode);
+    switch (keycode) {
+        case STR_TV:
+            if (record->event.pressed) {
+                register_code(KC_RCTL);
+                register_code(KC_RSFT);
+                register_code(KC_F13);
+            } else {
+                unregister_code(KC_F13);
+                unregister_code(KC_RSFT);
+                unregister_code(KC_RCTL);
+            }
+            return false;
+        }
+    return true;
+};
--- users/jians/jians.h
+++ users/jians/jians.h
@@ -1,3 +1,8 @@
 #pragma once
+#include QMK_KEYBOARD_H
 
 #include "wrappers.h"
+
+enum custom_keycodes {
+    STR_TV = QK_KB_0
+};
